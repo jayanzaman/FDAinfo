b571d0ca2f56b74f47b7fd702be1897c2150230c
Jayan Zaman <jayanzaman@gmail.com>
2016-11-25 15:23:12 -0500

    first ajax call

---
+++ public/js/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    console.log('script loaded.')
+
+    var drugName = "Abilify";
+    var callFDA = function() {
+        $.ajax({
+            url: 'https://api.fda.gov/drug/label.json?search=openfda.brand_name.%22' + drugName + '%22',
+            dataType: 'json',
+            success: function(data) {
+                parseData(data);
+            }
+        })
+    }
+    callFDA();
+
+    var parseData = function(data) {
+
+        var manufacturerName = data.results[0].openfda.manufacturer_name[0];
+        var stringData = JSON.stringify(data.results[0]);
+        console.log(data)
+            // console.log("Drug Info: " + stringData)
+            // console.log("Manufaturer" + manufacturerName)
+
+
+
+
+
+
+
+        // console.log(data.results[0].dosage_and_administration[0])
+        var dosageTable = data.results[0].dosage_and_administration_table[0];
+        var $dosageTable = $('.dosageTable');
+        $dosageTable.append(dosageTable);
+        $('table').addClass('bordered highlight');
+        var dosageTable2 = data.results[0].dosage_and_administration_table[1];
+        var $dosageTable2 = $('.dosageTable');
+        $dosageTable2.append(dosageTable2);
+        $('table').addClass('bordered highlight');
+    };
+
+
+})
