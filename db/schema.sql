a5026141bea6158f969858125d394d651895ea07
Jayan Zaman <jayanzaman@gmail.com>
2016-11-29 16:00:10 -0500

    afternoon of Nov 29

--- db/schema.sql
+++ db/schema.sql
@@ -7,4 +7,20 @@ CREATE TABLE users (
   password_digest VARCHAR(255)
 );
 
+DROP TABLE IF EXISTS druginfo;
 
+CREATE TABLE druginfo (
+  brand_name VARCHAR(255),
+  rx_date VARCHAR(10),
+  refill INT,
+  refill_date VARCHAR(10),
+  prescribing_dr VARCHAR(255),
+  expiration_date VARCHAR(10),
+  quantity INT,
+  no_of_pills INT,
+  inst_no_tabs INT,
+  inst_tabs_daily INT,
+  users_email VARCHAR(255),
+  users_id INT
+
+)
