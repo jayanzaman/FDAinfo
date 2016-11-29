abc4e57722a5a3c990ad7f309579fefa6c8702de
Jayan Zaman <jayanzaman@gmail.com>
2016-11-29 10:38:19 -0500

    edited professionals

--- public/js/script.js
+++ public/js/script.js
@@ -71,36 +71,6 @@ $(document).ready(function() {
 
     }
 
-    $(function() {
-        var availableTags = [
-            "ActionScript",
-            "AppleScript",
-            "Asp",
-            "BASIC",
-            "C",
-            "C++",
-            "Clojure",
-            "COBOL",
-            "ColdFusion",
-            "Erlang",
-            "Fortran",
-            "Groovy",
-            "Haskell",
-            "Java",
-            "JavaScript",
-            "Lisp",
-            "Perl",
-            "PHP",
-            "Python",
-            "Ruby",
-            "Scala",
-            "Scheme"
-        ];
-        $("#autocomplete-input").autocomplete({
-            source: availableTags
-        });
-    });
-
 
 
 
