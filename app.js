826be91b4e15658a6056b5c369c60a81573f6c16
Jayan Zaman <jayanzaman@gmail.com>
2016-12-01 07:01:31 -0500

    Thursday early morning

--- app.js
+++ app.js
@@ -32,25 +32,20 @@ app.get("/", function(req, res) {
     var logged_in;
     var email;
     var id;
-
     if (req.session.user) {
         logged_in = true;
         email = req.session.user.email;
         id = req.session.user.id;
     }
-
     var data = {
         "logged_in": logged_in,
         "email": email,
         "id": id
     }
-
     res.render('login', data);
 });
 app.post('/login', function(req, res) {
     var data = req.body;
-
-
     db.one(
         "SELECT * FROM users WHERE email = $1", [data.email]
     ).catch(function() {
@@ -66,12 +61,6 @@ app.post('/login', function(req, res) {
         });
     });
 });
-
-app.get("/dashboard", function(req, res) {
-
-    res.render('dashboard')
-})
-
 app.get("/signup", function(req, res) {
     res.render('signup')
 })
@@ -86,34 +75,9 @@ app.post('/signup', function(req, res) {
         })
     });
 })
-
-
-app.get("/druginfo", function(req, res) {
-    res.render('druginfo')
+app.get("/dashboard", function(req, res) {
+    res.render('dashboard')
 })
-
-app.get("/visuals", function(req, res) {
-    res.render('visuals')
-})
-app.get("/professionals", function(req, res) {
-    res.render('professionals')
-})
-
-app.get("/settings", function(req, res) {
-    var logged_in;
-    var email;
-
-    if (req.session.user) {
-        logged_in = true;
-        email = req.session.user.email;
-    }
-    var data = {
-        "logged_in": logged_in,
-        "email": email
-    }
-    res.render('settings')
-})
-
 app.get("/dashboard/:id", function(req, res) {
     var logged_in;
     var email;
@@ -129,16 +93,17 @@ app.get("/dashboard/:id", function(req, res) {
         "email": email,
         "id": id
     }
-    db.any("SELECT * from druginfo WHERE druginfo.users_email = $1 ", [data.email])
-        .then(function(data) {
-            console.log(data)
-            res.render('dashboard', {
-                drugs: data
-            })
-
+    db.any("SELECT * FROM druginfo WHERE druginfo.users_email = $1 ", [data.email])
+        .then(function(drugs) {
+            data = {
+                "logged_in": logged_in,
+                "email": email,
+                "id": id,
+                "drugs": drugs
+            }
+            console.log(drugs)
+            res.render('dashboard', data)
         })
-
-
 })
 
 app.post('/dashboard/:id', function(req, res) {
@@ -157,6 +122,123 @@ app.post('/dashboard/:id', function(req, res) {
     db.none("INSERT INTO druginfo(drug_name, rx_date, pickup_date, exp_date, prescribing_dr, dr_phone, users_email, users_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8)", [newRx.drug_name, newRx.rx_date, newRx.pickup_date, newRx.exp_date, newRx.prescribing_dr, newRx.dr_phone, email, id])
         .then(function(data) {
             res.redirect('/dashboard/' + id)
+        })
+})
+
+app.get("/druginfo", function(req, res) {
+    res.render('druginfo')
+})
+app.get("/druginfo/:id", function(req, res) {
+    var logged_in;
+    var email;
+    var id;
+
+    if (req.session.user) {
+        logged_in = true;
+        email = req.session.user.email;
+        id = req.session.user.id;
+    }
+    var data = {
+        "logged_in": logged_in,
+        "email": email,
+        "id": id
+    }
+    db.any("SELECT * FROM druginfo WHERE druginfo.users_email = $1 ", [data.email])
+        .then(function(drugs) {
+            data = {
+                "logged_in": logged_in,
+                "email": email,
+                "id": id,
+                "drugs": drugs
+            }
+            console.log(drugs)
+            res.render('druginfo', data)
+        })
+})
+app.get("/visuals", function(req, res) {
+    res.render('visuals')
+})
+app.get("/visuals/:id", function(req, res) {
+    var logged_in;
+    var email;
+    var id;
+
+    if (req.session.user) {
+        logged_in = true;
+        email = req.session.user.email;
+        id = req.session.user.id;
+    }
+    var data = {
+        "logged_in": logged_in,
+        "email": email,
+        "id": id
+    }
+    db.any("SELECT * FROM druginfo WHERE druginfo.users_email = $1 ", [data.email])
+        .then(function(drugs) {
+            data = {
+                "logged_in": logged_in,
+                "email": email,
+                "id": id,
+                "drugs": drugs
+            }
+            console.log(drugs)
+            res.render('visuals', data)
+        })
+})
+app.get("/professionals", function(req, res) {
+    res.render('professionals')
+})
+app.get("/professionals/:id", function(req, res) {
+    var logged_in;
+    var email;
+    var id;
+
+    if (req.session.user) {
+        logged_in = true;
+        email = req.session.user.email;
+        id = req.session.user.id;
+    }
+    var data = {
+        "logged_in": logged_in,
+        "email": email,
+        "id": id
+    }
+    db.any("SELECT * FROM druginfo WHERE druginfo.users_email = $1 ", [data.email])
+        .then(function(drugs) {
+            data = {
+                "logged_in": logged_in,
+                "email": email,
+                "id": id,
+                "drugs": drugs
+            }
+
+            res.render('professionals', data)
+        })
+})
+app.get("/settings/:id", function(req, res) {
+    var logged_in;
+    var email;
+    var id;
+
+    if (req.session.user) {
+        logged_in = true;
+        email = req.session.user.email;
+        id = req.session.user.id;
+    }
+    var data = {
+        "logged_in": logged_in,
+        "email": email,
+        "id": id
+    }
+    db.any("SELECT * FROM users WHERE users.email = $1 ", [data.email])
+        .then(function(user) {
+            data = {
+                "logged_in": logged_in,
+                "email": email,
+                "id": id,
+                "user": user
+            }
+            res.render('settings', data)
         })
 })
 
