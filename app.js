a3fa58b9ba40a72ee35387ecce67acf4dd4738ec
Jayan Zaman <jayanzaman@gmail.com>
2017-02-10 11:04:36 -0500

    added pg

--- app.js
+++ app.js
@@ -29,6 +29,20 @@ app.use(session({
     }
 }))
 
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+
 var db = pgp(process.env.DATABASE_URL || 'postgres://jayanzaman@localhost:5432/fda_open');
 
 app.get("/", function(req, res) {
